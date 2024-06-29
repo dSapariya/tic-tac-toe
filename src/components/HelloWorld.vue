bdc3ef8ac5806220330f90a59b3ae6cb6edfccb6
Dixa Sapariya <dixasapariya786@gmail.com>
2024-06-29 21:14:56 +0530

    minor changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,11 @@
   <h2>Tic-Tac-Toe</h2>
   <h3 v-if="!gameStage">Current Player : {{ currentPlayer ? "X" : "0" }}</h3>
   <h3 v-else>Player {{ winner }} Won the Match</h3>
+  
   <h3 v-if="count > matrixSize * matrixSize - 1 && !gameStage" style="color: red">
     No one is Won
   </h3>
+
   <div class="container">
     <div class="player-card">
       <h1>Player X</h1>
